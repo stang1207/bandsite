95a0c17705214bfb315f7e4c54e21e8ff79d2a6d
Lok <siulokt1207@gmail.com>
2022-02-03 14:52:45 -0500

    added animations and js file

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+//Section fade in animations related
+const sections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+sections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
